3a22dd69c070c6bfa69fffe83d4bce6966dfe106
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-09-07 18:33:06 +0300

    pnotify styles light

--- src/js/pnotify.js
+++ src/js/pnotify.js
@@ -2,9 +2,9 @@ import { alert, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
+const { defaults } = require('@pnotify/core');
 
 defaultModules.set(PNotifyMobile, {});
 
-// alert({
-//   text: 'Notice me, senpai!'
-// });
+defaults.delay = '2500';
+defaults.maxTextHeight = null;
